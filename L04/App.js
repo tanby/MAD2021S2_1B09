c8d07521b462d643e891f1760d15cb3c18e19062
Boon Yuen Tan <tan_boon_yuen@ichat.sp.edu.sg>
2020-11-11 11:09:36 +0800

    Lesson 4 starter demo

---
+++ L04/App.js
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+// from the PPT slide
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+
+export default class App extends React.Component {
+  // render UI
+  render(){
+    // use JSX
+    return (
+      <View>
+        <Text>Hello 1B09</Text>
+      </View>
+    );
+  }
+}
