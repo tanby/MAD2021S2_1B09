dac9252f15b7aee6fffa67ec3ff23f8be491c2fa
Boon Yuen Tan <tan_boon_yuen@ichat.sp.edu.sg>
2020-11-11 11:53:36 +0800

    lesson 4 add state and props

--- L04/App.js
+++ L04/App.js
@@ -10,13 +10,35 @@ import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 
 export default class App extends React.Component {
+  // demo state
+  state = {
+    example: 100
+  }
   // render UI
   render(){
     // use JSX
     return (
       <View>
-        <Text>Hello 1B09</Text>
+        <Text>Hello 1B09, this is {this.state.example}</Text>
+        <ABC dit="1b09" />
+        <ABC dit="3b99" />
       </View>
     );
   }
 }
+
+// create a separate Component to be reused
+class ABC extends React.Component {
+  // render UI
+  // demo props
+  render(props){
+    // use JSX
+    return (
+      <View>
+        <Text>this is property</Text>
+        <Text>{this.props.dit}</Text>
+      </View>
+    );
+  }
+}
+export {ABC};
